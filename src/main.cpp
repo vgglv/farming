48e4ee6c101685f877f03b1793c84e0b6fe762a0
vgglv <ripykt@gmail.com>
2024-12-23 22:44:35 +0900

    wip

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "fmt/format.h"
+#include "json/json.h"
+
+int main() {
+    fmt::print("Welcome to Farming game!");
+    return 0;
+}
---
+++ src/types.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+struct Plant {
+    std::string name;
+    std::string description;
+    float cost = 0.f;
+    int time = 0;
+};
